f8c9c1dd5eb3a4bb4c136660a95eafcee9d4a83f
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-02-28 04:59:42 +0300

    implementation

---
+++ index.js
@@ -0,0 +1,21 @@
+// Function implementation
+function solution (A, K) {
+    //check the length of rotation
+    const N = A.length;
+    if (N === 0) {
+      return A;
+    }
+    K = K % N;
+    if (K === 0) {
+      return A;
+    }
+    //no rotation
+    const rotatedArray = [];
+  
+    for (let i = 0; i < N; i++) {
+      const newValue = (i + K) % N;
+      rotatedArray[newValue] = A[i];
+    }
+    return rotatedArray;
+  
+  }
