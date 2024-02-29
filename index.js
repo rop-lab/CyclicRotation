f03537a5d2c014b3ef2c64b901376c727b15c48a
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-02-29 16:47:20 +0300

    DONE

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ function solution(A, K) {
   }
   
   // Example usage
-  const exampleUsage1 = solution([3, 8, 9, 7, 6], 3);
-  console.log(exampleUsage1); // Output: [9, 7, 6, 3, 8]
+  const example1 = solution([3, 8, 9, 7, 6], 3);
+  console.log(example1); // Output: [9, 7, 6, 3, 8]
   
-  const exampleUsage2 = solution([0, 0, 0], 1);
-  console.log(exampleUsage2); // Output: [0, 0, 0]
+  const example2 = solution([0, 0, 0], 1);
+  console.log(example2); // Output: [0, 0, 0]
   
-  const exampleUsage3 = solution([1, 2, 3, 4], 4);
-  console.log(exampleUsage3); // Output: [1, 2, 3, 4]
+  const example3 = solution([1, 2, 3, 4], 4);
+  console.log(example3); // Output: [1, 2, 3, 4]
   
